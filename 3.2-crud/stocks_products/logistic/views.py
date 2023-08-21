9673331286a9d5c7f23e897a645068f58fda2b7e
TannyKorry <tannykorry@gmail.com>
2023-08-21 20:56:46 +0300

    Test 21.08.23 20:56

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -32,4 +32,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view()
 def sample_view(request):
-    return Response({"message": 'И снова привет!'})
+    return Response({"message": 'Test 21.08.23 20:56'})
